ef75bf55847a3b20e9dee224bc54bc9e9ed94127
nmsms <nahuelmatiasbenitez@gmail.com>
2023-09-08 17:41:03 -0300

    Actualizacion Lamparita

--- Lamparita/src/Lamparita.java
+++ Lamparita/src/Lamparita.java
@@ -1,5 +1,28 @@
 public class Lamparita {
     public static void main(String[] args) throws Exception {
         System.out.println("Vamos a programar una lamparita! ");
+        Lamparita lamparita = new Lamparita();
+        lamparita.switchLamparita();
+        System.out.println("El estado de la lamparita es:" + lamparita.getEstadoLamparita());
+        lamparita.switchLamparita();
+        System.out.println("El estado de la lamparita es:" + lamparita.getEstadoLamparita());
     }
+
+    private boolean estadoLamparita = false;
+
+    public void switchLamparita(){
+        estadoLamparita = !estadoLamparita;
+    }
+
+    public void apagarLamparita(){
+        estadoLamparita = false;
+    }
+    
+    public void prenderLamparita(){
+        estadoLamparita = true;
+    }
+    public boolean getEstadoLamparita(){
+        return estadoLamparita;
+    }
+
 }
